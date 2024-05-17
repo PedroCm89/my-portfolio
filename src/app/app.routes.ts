7700fbde0ae5f85e2a31429035cc60adca00babb
pedro <pedro.cm.1989@gmail.com>
2024-05-17 21:44:48 +0200

    gorring in header and delate old commponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
 
 
-export const routes: Routes = [
 
-    { path: '', component: HomeComponent },
-    { path: 'Contact', component: ContactComponent},
-    { path: 'Projects', component:ProjectsComponent},
-    { path: '**', component:HomeComponent}
-];
+export const routes: Routes = [];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
