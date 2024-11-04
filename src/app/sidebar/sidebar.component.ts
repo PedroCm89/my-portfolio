694bceecda6b9513137f386413efd07a610ecc3d
pedro <pedro.cm.1989@gmail.com>
2024-11-04 15:50:17 +0100

    add iconos and links

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,33 @@
+import { NgFor } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent  implements OnInit, AfterViewInit{
 
   @ViewChild('asTitle') asTitle: ElementRef | undefined;
+  links: Array<any> =[
+    {
+      link: 'linkedin.com/in/pedro-carrasco-mora-08a273254',
+      icon: '<i class="uil uil-linkedin"></i>'
+    },
+    {
+      link: 'https://github.com/PedroCm89',
+      icon: '<i class="uil uil-github"></i>'
+    },
+    {
+      link: 'mailto:pecamo.1989@gamil.com',
+      icon: '<i class="uil uil-envelope"></i>'
+    },
+   
+     
+  ];
 
   constructor(){
 
@@ -18,7 +35,6 @@ export class SidebarComponent  implements OnInit, AfterViewInit{
 
   ngOnInit():void{
 
-    
   }
 
   ngAfterViewInit(): void{
@@ -43,6 +59,15 @@ export class SidebarComponent  implements OnInit, AfterViewInit{
 
       
   };
+ 
 
 }
+
+//class LinkModel {
+  //link: string;
+  //icon: string;
+
+//}
+
+
 
